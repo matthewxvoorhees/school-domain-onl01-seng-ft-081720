d6f2d3e4516a9701acd5d9e3f98e81f6592421cf
Matthew Crnich <matthew.crnich@gmail.com>
2020-08-26 20:01:50 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,41 @@
-# code here!
+class School
+  attr_reader :roster
+
+  def initializei(name)
+    @name = name
+  end
+  
+   ROSTER = []
+  
+  def roster=(roster)
+    @roster = roster
+    ROSTER << ROSTER
+  end
+  
+end
+
+
+
+
+
+
+# class Book
+#   attr_accessor :author, :page_count  # remove the attr_accessor for genre
+#   attr_reader :title, :genre  # add an attr_reader for genre
+ 
+#   GENRES = []
+ 
+#   def initialize(title)
+#     @title = title
+#   end
+ 
+#   def turn_page
+#     puts "Flipping the page...wow, you read fast!"
+#   end
+ 
+#   # create the writer for genre and add the logic for the class constant
+#   def genre=(genre)
+#     @genre = genre
+#     GENRES << genre 
+#   end
+# end
